c54f0325b4ce6148a0ac416285bccef3ab008862
2732ii2 <mo.anas123khan@gmail.com>
2023-10-10 16:18:46 +0530

    10_oct updation

--- quizapp/src/Component/AdDash.js
+++ quizapp/src/Component/AdDash.js
@@ -6,9 +6,36 @@ import { Doughnut,Bar ,Line} from "react-chartjs-2";
 import { useNavigate } from 'react-router-dom';
 export default function AdDash() {
     var [err,seterr]=useState("");
+    var per_arr=[];
+    var user_arr=[]
+    var [selec_sub, setsel] = useState("Verbal");
+    var [spec_id,setid]=useState(0);
+    console.log(spec_id);
     var [arr, setarr] = useState([]);
     var [arr2, setarr_2] = useState([]);
     console.log(arr,arr2);
+    if (arr.length!=0){
+      arr.forEach((e,i)=>{
+        if (i<arr.length-1){
+          console.log(e.name ? JSON.parse(e.name) : "no there");
+        } 
+        else{
+          console.log("by");
+        }
+      })
+    }
+    if (arr2.length!=0){
+      
+     var d= arr2.filter((e, i) => {
+        if (e.sub_name == selec_sub) {
+          console.log(e.percentage);
+          per_arr.push(e.percentage);
+          user_arr.push(e.name_of_user);
+          return {per:e.percentage};
+        }
+      });  
+      console.log(d,per_arr);
+    }
     var list_s = [
       { name: "Ashad", subject: "Verbal" },
       { name: "Amaan", subject: "Reasoning" },
@@ -41,7 +68,7 @@ export default function AdDash() {
         var { data } = await axios.get(
           "https://easyquizy.onrender.com/getusers"
         );
-        console.log(data);
+        // console.log(data);
         setarr(data);
         var data2 = await axios.get("http://localhost:9800/getresult");
         setarr_2(data2.data);
@@ -54,11 +81,14 @@ export default function AdDash() {
       }
     }
     const data2 = {
-      labels: ["green ", "red", "orange", "white", "black","less"],
+      labels:
+        arr2.length !== 0
+          ? user_arr
+          : ["green ", "red", "orange", "white", "black"],
       datasets: [
         {
           label: "My First Dataset",
-          data: [250, 50, 200, 70, 100],
+          data: arr2.length !== 0 ? per_arr : [0,0,0,0,0],
 
           backgroundColor: [
             "rgb(0, 242, 255)",
@@ -73,12 +103,15 @@ export default function AdDash() {
       ],
     };
     const data3 = {
-      labels: ["green ", "red", "orange", "white", "black","less"],
+      labels:
+        arr2.length !== 0
+          ? user_arr
+          : ["green ", "red", "orange", "white", "black"],
       datasets: [
         {
           fill: true,
           label: "My First Dataset",
-          data: [250, 50, 200, 70, 100,207],
+          data: arr2.length !== 0 ? per_arr : [250, 50, 200, 70, 100],
           // backgroundColor: [
           //   "rgb(0, 242, 255)",
           //   "rgb(14, 164, 233",
@@ -91,72 +124,104 @@ export default function AdDash() {
           // borderColor: "rgb(53, 162, 235)",
           backgroundColor: "rgba(53, 162, 235, 0.5)",
         },
-        {
-          fill: true,
-          label: "My First Dataset",
-          data: [150, 20, 100, 40, 30, 43],
+        // {
+        //   fill: true,
+        //   label: "My First Dataset",
+        //   data: [150, 20, 100, 40, 30, 43],
 
-          // backgroundColor: [
-          //   "rgb(0, 242, 255)",
-          //   "rgb(14, 164, 233",
-          //   "rgb(37, 119, 243)",
-          //   "rgb(0, 242, 255)",
-          //   "rgb(14, 164, 233",
-          // ],
-          hoverOffset: 4,
-          backgroundColor: "rgba(53, 162, 235, 0.4)",
-          borderColor: "rgb(53, 242, 255)",
-        },
+        //   // backgroundColor: [
+        //   //   "rgb(0, 242, 255)",
+        //   //   "rgb(14, 164, 233",
+        //   //   "rgb(37, 119, 243)",
+        //   //   "rgb(0, 242, 255)",
+        //   //   "rgb(14, 164, 233",
+        //   // ],
+        //   hoverOffset: 4,
+        //   backgroundColor: "rgba(53, 162, 235, 0.4)",
+        //   borderColor: "rgb(53, 242, 255)",
+        // },
       ],
     };
     useEffect(()=>{
       caller();
     },[])
-  return (
-    <div id="admindash">
-      <div id="sidebar">
-        <h1 id="sidh_1">Admin Dashboard</h1>
-        <div id="coloured">
-          <div id="samehead">Home</div>
-          <div onClick={()=>{
-            navi("/");
-          }} id="samehead">Logout</div>
+  try{
+    return (
+      <div id="admindash">
+        <div id="sidebar">
+          <h1 id="sidh_1">Admin Dashboard</h1>
+          <div id="coloured">
+            <div id="samehead">Home</div>
+            <div
+              onClick={() => {
+                navi("/");
+              }}
+              id="samehead"
+            >
+              Logout
+            </div>
+          </div>
+        </div>
+        <div id="mainbar">
+          <div id="uppergraph">
+            <div id="sameuppers">
+              <Line data={data3} />
+            </div>
+            <div id="sameuppers">
+              {" "}
+              <Doughnut data={data} />
+            </div>
+          </div>
+          <div id="downs">
+            <div id="lists">
+              <h4 id="h_4_test">List of Test</h4>
+              <div id="actualdowns">
+                {arr.length != 0 ? (
+                  arr.map((e,i)=>{
+                  var c=(e.name ? JSON.parse(e.name)[1] : "no there");
+
+                    return (
+                      <div
+                        onClick={() => {
+                          console.log(c.subject);
+                          setsel(c.subject);
+                          setid(i);
+                        }}
+                        id={i === spec_id ? "ls0_" : "null"}
+                        key={i}
+                        className="ls_"
+                      >
+                        <div>{c.name}</div>
+                        <div>{c.subject}</div>
+                      </div>
+                    );
+                  })
+                ) : (
+                  list_s.map((e, i) => {
+                    return (
+                      <div
+                        id={`ls${i}_`}
+                        key={i}
+                        className="ls_"
+                      >
+                        <div>{e.name}</div>
+                        <div>{e.subject}</div>
+                      </div>
+                    );
+                  })
+                )}
+              </div>
+            </div>
+            <div id="samedowns">
+              <Bar data={data2} />
+            </div>
+          </div>
         </div>
       </div>
-      <div id="mainbar">
-        <div id="uppergraph">
-          <div id="sameuppers">
-            <Line data={data3} />
-          </div>
-          <div id="sameuppers">
-            {" "}
-            <Doughnut data={data} />
-          </div>
-        </div>
-        <div id="downs">
-          <div id="lists">
-                <h4 id='h_4_test'>List of Test</h4>
-                <div id='actualdowns'>
-                  {
-                    list_s.map((e,i)=>{
-                      return (
-                        <div id={`ls${i}_`} key={i} className="ls_">
-                          <div>{e.name}</div>
-                          <div>{e.subject}</div>
-                        </div>
-                      );
-                    })
-                  }
-
-                </div>
-
-          </div>
-          <div id="samedowns">
-            <Bar data={data2} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
+  catch(e){
+    console.log(e.message);
+  }
 }
 
